1d436a2b0ccb0b8f56da4daed502f8aba785af7e
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-14 17:05:26 +0200

    Add label to ParameterValue and add ParameterValue to value config

--- frontend/src/metabase-types/api/parameters.ts
+++ frontend/src/metabase-types/api/parameters.ts
@@ -65,7 +65,7 @@ export type ValuesQueryType = "list" | "search" | "none";
 export type ValuesSourceType = null | "card" | "static-list";
 
 export interface ValuesSourceConfig {
-  values?: string[];
+  values?: string[] | ParameterValue;
   card_id?: CardId;
   value_field?: unknown[];
 }
@@ -91,7 +91,7 @@ export type StructuredParameterDimensionTarget = [
 ];
 
 export type ParameterValueOrArray = string | number | Array<any>;
-export type ParameterValue = [RowValue];
+export type ParameterValue = [RowValue] | [RowValue, string];
 
 export type ParameterValuesMap = Record<
   ParameterId,
